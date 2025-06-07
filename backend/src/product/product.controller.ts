7093f6f8a2bff978a51a3020f8caa7843cef47a4
Yan Lucas <yanlucascarvalho18@gmail.com>
2025-06-07 16:00:52 -0300

    feat: add create product logic

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { ProductService } from "./product.service";
+import { CreateProductDto } from "./dto/create-product-dto";
 
 @Controller("product")
 export class ProductController {
-    constructor() {}
+    constructor(private readonly productService: ProductService) {}
+
+    @Post()
+    async createProduct(@Body() productData: CreateProductDto) {
+        return this.productService.createProduct(productData);
+    }
 }
--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Product } from "./entities/product.entity";
+import { CreateProductDto } from "./dto/create-product-dto";
 
 @Injectable()
 export class ProductService {
@@ -9,5 +10,27 @@ export class ProductService {
         @InjectRepository(Product)
         private readonly productRepository: Repository<Product>,
     ) {}
-    async createProduct(productData) {}
+    async createProduct(productData: CreateProductDto) {
+        const { name, sku, price } = productData;
+        const existingProduct = await this.findProductBySku(sku);
+        if (existingProduct) {
+            throw new ConflictException(`O produto com SKU ${sku} já existe.`);
+        }
+        try {
+            const newProduct = this.productRepository.create({
+                name,
+                sku,
+                price,
+            });
+            return this.productRepository.save(newProduct);
+        } catch (error) {
+            throw new InternalServerErrorException("Erro inesperado ao criar o produto.");
+        }
+    }
+
+    async findProductBySku(sku: string) {
+        return this.productRepository.findOne({
+            where: { sku },
+        });
+    }
 }
