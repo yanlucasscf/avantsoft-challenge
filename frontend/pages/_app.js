7e0dc1547e9f1911f4d81c7a6188d7730d2c9c57
Yan Lucas <yanlucascarvalho18@gmail.com>
2025-06-09 19:23:52 -0300

    feat: add navbar

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,10 @@
 import "@/styles/globals.css";
 import { Provider } from "@/components/ui/provider";
+import NavBar from "./components/NavBar";
 export default function App({ Component, pageProps }) {
   return (
     <Provider>
+      <NavBar />
       <Component {...pageProps} />;
     </Provider>
   );
